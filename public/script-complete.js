bd97eeb6954e139441841b3447163beb8952c556
gabriellelamarrlemee <lamarrlemee.g@husky.neu.edu>
2017-03-17 11:02:50 -0500

    Second commit

--- public/script-complete.js
+++ public/script-complete.js
@@ -1,20 +1,20 @@
 
 // Toggling can also be done through javascript
 $('#toggle').on('click', function (){
-    // ** complete this section **
+    $(this).button('toggle');
   });
 
 // Activating the loading state
 $('#loadingButton').on('click', function(){
-  // ** complete this section **
+  var $btn = $(this).button('loading');
   console.log('click');
   //simulating a timeout
   setTimeout(function () {
-      // ** return the button to its original state **
+      $btn.button('reset');
   }, 1000);
 });
 
 // Swap out the text
 $('#newText').on('click', function (){
-    // ** make the button read "Thank you!" when it is clicked **
+    $(this).attr('data-complete-text', 'Thank you!').button('complete');
   });
